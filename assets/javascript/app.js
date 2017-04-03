403d185d8bc43645dac6043a8ab0c56d1a834e70
Michael Emmanuel <michael.emmanuel732@gmail.com>
2017-04-03 13:27:08 -0400

    need to fix still state

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,71 +3,65 @@
 // var query = "&q=" + topic  
 // var topic = ""
 
-var topics = ['cheesecake', 'ice cream', 'cookies', 'brownies', 'cupcakes', 'lollipop', 'pie', 'gingerbread', 'doughnuts', 'pudding'];
+$(function(){ //anything in here will appear on page load
+  populateButtons(searchArray,'searchButton','#buttonsArea')
+  console.log("Page Loaded");
+})
 
-function dessertGifs() {
-  var desserts = $(this).attr('data-name')
-  var queryUrL = "http://api.giphy.com/v1/gifs/search?q=" + desserts + "&api_key=dc6zaTOxFJmzC&limit=10";
+var searchArray = ['Dog','Cat','Bird'];
 
-  $.ajax({
-    url: queryUrL,
-    method: "GET"
-  }).done(function(response){
-    var results = response.data;
+function populateButtons(searchArray,classToAdd,areaToAddTo){
+  $(areaToAddTo).empty();
+  for(var i=0;i<searchArray.length;i++){
+    var a = $('<button>');
+    a.addClass(classToAdd);
+    a.attr('data-type',searchArray[i])
+    a.text(searchArray[i]);
+    $(areaToAddTo).append(a)
+  }
+}
 
-    for(var i = 0; i < results.length; i++) {
-      var dessertDiv = $("<div></div>");
-      var rate = results[i].rate;
-      var stopAnimate = results[i].images.fixed_width_still.url;
-      var resumeAnimate = results[i].images.fixed_width.url;
-      var p = $("<p>").text("Rating: " + rate);
+$(document).on('click','.searchButton',function(){
+  var type = $(this).data('type');
+  var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_key=dc6zaTOxFJmzC&limit=10';
+  $.ajax({url:queryURL,method:'GET'})
+    .done(function(response){
+      for(var i=0;i<response.data.length;i++){
+        var searchDiv = $('<div class="search-item">');
+        var rating = response.data[i].rating;
+        var p = $('<p>').text('Rating: '+rating);
+        var animated = response.data[i].images.fixed_height.url;
+        var still = response.data[i].images.fixed_height_still.url;
+        var image = $('<img>');
+        image.attr('src',still);
+        image.attr('data-still',still);
+        image.attr('data-animated',animated);
+        image.attr('data-state','still');
+        image.addClass('searchImage');
+        searchDiv.append(p);
+        searchDiv.append(image);
+        $('#searches').append(searchDiv);
+      }
+    })
+})
 
-      var dessertGif = $("<img>");
-      dessertGif.attr("src", stopAnimate).attr("data-still", stopAnimate).attr("data-animate", resumeAnimate).attr("data-state", "still").attr("class", "gif");
-      dessertDiv.prepend(p);
-      dessertDiv.prepend(dessertGif);
-      $("#dessert-gifs").prepend(dessertGif);
+$(document).on('click','.searchImage',function(){
+  var state = $(this).data('state');
+  if(state == 'still'){
+    $(this).attr('src',$(this).data('animated'));
+    $(this).attr('data-state','animated');    
+  }else{
+    $(this).attr('src',$(this).data('still'));
+    $(this).attr('data-state','still'); 
+  }
+})
 
-    };
-  });
-};
-
-function createButtons() {
-  $('#default-buttons').empty();
-
-  for(var 1 = 0; i < topics.length; i++) {
-    var btn = $("<button>");
-    btn.addClass('desserts');
-    btn.attr('data-name', topics[i]);
-    btn.text(topics[i]);
-    $('#default-buttons').append(btn);
-  };
-
-  $('#addDessert').on('click', function(event){
-    event.preventDefault();
-    var dessertInput = $("#dessert-input").val().trim();
-    topics.push(dessertInput);
-
-    createButtons();
-  });
-};
-
-$(document).on("click", ".desserts", dessertGifs);
-
-  createButtons();
-
-  $(document).on('click', '.gif', function(){
-    var state = $(this).attr('data-state');
-
-    if (state == 'still') {
-      var animate = $(this).attr("data-animate")
-      $(this).attr("data-state", "animate").attr("src", animate)
-    }else{
-      var still = $(this).attr("data-still")
-      $(this).attr("data-state", "still").attr("src", still);
-    };
-  });
-
+$('#addSearch').on('click',function(){
+  var newSearch = $('input').eq(0).val();
+  searchArray.push(newSearch);
+  populateButtons(searchArray,'searchButton','#buttonsArea');
+  return false;
+})
 
 
 
